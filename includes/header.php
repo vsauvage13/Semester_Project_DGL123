7a9c05f656451631af32da77090ea645184ac6c5
vsauvage13 <90713179+vsauvage13@users.noreply.github.com>
2023-10-15 19:31:37 -0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -9,26 +9,31 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <a class="navbar-brand" href="#!">Plant Store</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                                <li><a class="dropdown-item" href="products.php">All Products</a></li>
                                 <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                                <!-- TODO Link the below pages to a filter on the products page - implement php here -->
+                                <li><a class="dropdown-item" href="products.php">Popular Items</a></li>
+                                <li><a class="dropdown-item" href="products.php">New Arrivals</a></li>
                             </ul>
-                        </li>
+                        </li> 
                     </ul>
                     <form class="d-flex">
-                        <button class="btn btn-outline-dark" type="submit">
+                        <ul class="navbar-nav me-auto ">
+                        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                        </ul>
+                        <button class="btn btn-outline-dark" type="submit" formaction="cart.php">
                             <i class="bi-cart-fill me-1"></i>
                             Cart
+                            <!-- TODO  Below make number update to cart number using PHP -->
                             <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
                         </button>
                     </form>
